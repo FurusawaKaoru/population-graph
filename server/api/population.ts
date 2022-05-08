78526edbb1e64d92dff69ea5fbac0abe4c9192fb
KaoruFurusawa <kaoru.furusawa@yourmystar.jp>
2022-05-08 22:44:28 +0900

    add .env

--- server/api/population.ts
+++ server/api/population.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async () => {
   const cityCode = '-'
   const response = await axios.get(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}&cityCode=${cityCode}`, {
     headers: {
-      'X-API-KEY': 'BcziaxCTfsalIUzxiiZO8rtr6GAbG0sQjPSvc47F',
+      'X-API-KEY': import.meta.env.VITE_RESAS_API_KEY,
     },
   })
   return response.data.result
--- server/api/prefectures.ts
+++ server/api/prefectures.ts
@@ -1,10 +1,9 @@
 import axios from 'axios'
-import {name} from "ci-info";
 
 export default defineEventHandler(async () => {
   const response = await axios.get('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
     headers: {
-      'X-API-KEY': 'BcziaxCTfsalIUzxiiZO8rtr6GAbG0sQjPSvc47F',
+      'X-API-KEY': import.meta.env.VITE_RESAS_API_KEY,
     },
   })
   return response.data.result.map((record: { prefCode: number, prefName: string }) => {
