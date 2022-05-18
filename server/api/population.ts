34c13fc4291448c91dce5a86ee142bb9f8daf92e
KaoruFurusawa <kaoru.furusawa@yourmystar.jp>
2022-05-19 08:46:44 +0900

    feat api type

--- server/api/population.ts
+++ server/api/population.ts
@@ -1,11 +1,33 @@
 import axios from 'axios'
 
+type Record = {
+  year: number
+  value: number
+  rate: number | undefined
+}
+
 export default defineEventHandler(async (event) => {
   const query = useQuery(event)
-  const response = await axios.get(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${query.prefCode}&cityCode=-`, {
-    headers: {
-      'X-API-KEY': process.env.RESAS_API_KEY,
+  const response = await axios.get<{
+    message: string | null,
+    result: {
+      boundaryYear: number,
+      data: {
+        label: string
+        data: Record[]
+      }[]
     },
-  })
-  return response.data.result.data.find((record: { label: string, data: [] }) => record.label === '総人口').data
+  }>(
+    `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${query.prefCode}&cityCode=-`,
+    {
+      headers: {
+        'X-API-KEY': process.env.RESAS_API_KEY,
+      },
+    })
+  const population = response.data.result.data.find((record) => record.label === '総人口')
+  if (population === undefined) {
+    // TODO: error throw
+    return []
+  }
+  return population.data
 })
--- server/api/prefectures.ts
+++ server/api/prefectures.ts
@@ -1,12 +1,17 @@
 import axios from 'axios'
 
+type Record = {
+  prefCode: number
+  prefName: string
+}
+
 export default defineEventHandler(async () => {
-  const response = await axios.get('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
+  const response = await axios.get<{ message: string | null, result: Record[] }>('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
     headers: {
       'X-API-KEY': process.env.RESAS_API_KEY,
     },
   })
-  return response.data.result.map((record: { prefCode: number, prefName: string }) => {
+  return response.data.result.map((record) => {
     return { code: record.prefCode, name: record.prefName }
   })
 })
